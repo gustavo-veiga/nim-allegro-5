09e45839345ef094847dbbf7135f335758794922
Gustavo Veiga <gustavo.veiga@iteris.com.br>
2021-08-16 11:56:35 -0300

    PhysFS

---
+++ src/allegro5/physfs.nim
@@ -0,0 +1,12 @@
+import private/library
+
+{.push importc, dynlib: library.physfsAddon.}
+proc al_set_physfs_file_interface(): void
+proc al_get_allegro_physfs_version(): cuint
+{.pop.}
+
+proc physfsFileInterface*(): void =
+  al_set_physfs_file_interface()
+
+proc allegroPhysfsVersion*(): uint =
+  return al_get_allegro_physfs_version()
