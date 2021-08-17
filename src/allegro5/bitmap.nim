6e9e5fb60ede54309ccd9aa1b999ec70943a1929
Gustavo Veiga <gustavo.veiga@iteris.com.br>
2021-08-17 17:14:28 -0300

    Bitmap

--- src/allegro5/bitmap.nim
+++ src/allegro5/bitmap.nim
@@ -3,17 +3,17 @@ import private/library, color
 type
   AllegroBitmap* = ptr object
   AllegroBitmapFlag* {.pure.} = enum
-    memoryBitmap          = 0x0001,
-    keepBitmapFormat      = 0x0002, # now a bitmap loader flag
-    forceLocking          = 0x0004, # no longer honoured
-    noPreserveTexture     = 0x0008,
-    alphaTest             = 0x0010, # now a render state flag
-    internalOpenGL        = 0x0020,
-    minLinear             = 0x0040,
-    magLinear             = 0x0080,
-    mipmap                = 0x0100,
-    noPremultipliedAlpha  = 0x0200, # now a bitmap loader flag
-    videoBitmap           = 0x0400,
+    memoryBitmap          = 0x0001
+    keepBitmapFormat      = 0x0002 # now a bitmap loader flag
+    forceLocking          = 0x0004 # no longer honoured
+    noPreserveTexture     = 0x0008
+    alphaTest             = 0x0010 # now a render state flag
+    internalOpenGL        = 0x0020
+    minLinear             = 0x0040
+    magLinear             = 0x0080
+    mipmap                = 0x0100
+    noPremultipliedAlpha  = 0x0200 # now a bitmap loader flag
+    videoBitmap           = 0x0400
     convertBitmap         = 0x1000
 
 {.push importc, dynlib: library.allegro.}
@@ -57,6 +57,22 @@ proc al_convert_bitmap(bitmap: AllegroBitmap): void
 proc al_convert_memory_bitmaps(): void
 {.pop.}
 
+proc bitmapFormat*(format: int): void =
+  al_set_new_bitmap_format(format.cint)
+
+proc bitmapFlags*(flags: int): void =
+  al_set_new_bitmap_flags(flags.cint)
+
+proc bitmapFormat*(): int =
+  return al_get_new_bitmap_format()
+
+proc bitmapFlags*(): int =
+  return al_get_new_bitmap_flags()
+
+proc addBitmapFlag*(flag: int): void =
+  al_add_new_bitmap_flag(flag.cint)
+
+
 proc newAllegroBitmap*(width, height: uint): AllegroBitmap =
   return al_create_bitmap(width.cint, height.cint)
 
@@ -87,3 +103,44 @@ proc pixel*(bitmap: AllegroBitmap, x, y: cint): AllegroColor =
 #[ Masking ]#
 proc maskToAlpha*(bitmap: AllegroBitmap, maskColor: AllegroColor): void =
   al_convert_mask_to_alpha(bitmap, maskColor)
+
+#[ Clipping ]#
+proc clippingRectangle*(x, y: int, width, height: uint): void =
+  al_set_clipping_rectangle(x.cint, y.cint, width.cint, height.cint)
+
+proc resetClippingRectangle*(): void =
+  al_reset_clipping_rectangle()
+
+proc clippingRectangle*(): tuple[x, y: int, width, height: uint] =
+  var xV, yV, wV, hV: cint = 0
+  al_get_clipping_rectangle(xV, yV, wV, hV)
+  return (xV.int, yV.int, wV.uint, hV.uint)
+
+#[ Sub bitmaps ]#
+proc createSubBitmap*(parent: AllegroBitmap, x, y: int, width, height: uint): AllegroBitmap =
+  return al_create_sub_bitmap(parent, x.cint, y.cint, width.cint, height.cint)
+
+proc isSubBitmap*(bitmap: AllegroBitmap): bool =
+  return al_is_sub_bitmap(bitmap)
+
+proc parentBitmap*(bitmap: AllegroBitmap): AllegroBitmap =
+  return al_get_parent_bitmap(bitmap)
+
+proc x*(bitmap: AllegroBitmap): int =
+ return al_get_bitmap_x(bitmap)
+
+proc y*(bitmap: AllegroBitmap): int =
+  return al_get_bitmap_y(bitmap)
+
+proc reparent*(bitmap, parent: AllegroBitmap, x, y: int, width, height: uint): void =
+  al_reparent_bitmap(bitmap, parent, x.cint, y.cint, width.cint, height.cint)
+
+#[ Miscellaneous ]#
+proc clone*(bitmap: AllegroBitmap): AllegroBitmap =
+  return al_clone_bitmap(bitmap)
+
+proc convert*(bitmap: AllegroBitmap): void =
+  al_convert_bitmap(bitmap)
+
+proc convertMemoryBitmaps*(): void =
+  al_convert_memory_bitmaps()
