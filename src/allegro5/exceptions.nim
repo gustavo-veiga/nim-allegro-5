f8f805775bf21c9d11184ba4946dd67bacbb1c6a
Gustavo Veiga <gustavo.veiga@iteris.com.br>
2021-12-10 00:26:36 -0300

    Alegro Exceptions

--- src/allegro5/exceptions.nim
+++ src/allegro5/exceptions.nim
@@ -1,13 +1,13 @@
 type
-  AllegroException* = object of Defect
+  AllegroException*                   = object of CatchableError
 
-  AllegroInstallException* = object of AllegroException
-  AllegroInstallFontException* = object of AllegroInstallException
-  AllegroInstallMouseException* = object of AllegroInstallException
-  AllegroInstallAudioException* = object of AllegroInstallException
-  AllegroInstallAcodecException* = object of AllegroInstallException
-  AllegroInstallSystemException* = object of AllegroInstallException
-  AllegroInstallTtfFontException* = object of AllegroInstallException
-  AllegroInstallJoystickException* = object of AllegroInstallException
-  AllegroInstallKeyboardException* = object of AllegroInstallException
-  
+  AllegroInstallException*            = object of AllegroException
+  AllegroInstallFontException*        = object of AllegroInstallException
+  AllegroInstallMouseException*       = object of AllegroInstallException
+  AllegroInstallAudioException*       = object of AllegroInstallException
+  AllegroInstallAcodecException*      = object of AllegroInstallException
+  AllegroInstallSystemException*      = object of AllegroInstallException
+  AllegroInstallTtfFontException*     = object of AllegroInstallException
+  AllegroInstallJoystickException*    = object of AllegroInstallException
+  AllegroInstallKeyboardException*    = object of AllegroInstallException
+  AllegroInstallTouchInputException*  = object of AllegroInstallException
